acdd612792e932e21e62f4a048d20e12a5ee17ab
christianmasa <christian@linkernetworks.com>
2022-01-12 12:06:52 +0800

    check image type on pygame render

--- data_utils.py
+++ data_utils.py
@@ -9,14 +9,18 @@ from image_converter import depth_to_array, to_rgb_array
 import math
 from visual_utils import draw_3d_bounding_box
 
-sys.path.append("/opt/carla-simulator/PythonAPI/carla/dist/carla-0.9.12-py3.7-linux-x86_64.egg")
+sys.path.append(
+    "/opt/carla-simulator/PythonAPI/carla/dist/carla-0.9.12-py3.7-linux-x86_64.egg"
+)
 
 import carla
 
 cfg = cfg_from_yaml_file("configs.yaml")
 
 MAX_RENDER_DEPTH_IN_METERS = cfg["FILTER_CONFIG"]["MAX_RENDER_DEPTH_IN_METERS"]
-MIN_VISIBLE_VERTICES_FOR_RENDER = cfg["FILTER_CONFIG"]["MIN_VISIBLE_VERTICES_FOR_RENDER"]
+MIN_VISIBLE_VERTICES_FOR_RENDER = cfg["FILTER_CONFIG"][
+    "MIN_VISIBLE_VERTICES_FOR_RENDER"
+]
 MAX_OUT_VERTICES_FOR_RENDER = cfg["FILTER_CONFIG"]["MAX_OUT_VERTICES_FOR_RENDER"]
 WINDOW_WIDTH = cfg["SENSOR_CONFIG"]["DEPTH_RGB"]["ATTRIBUTE"]["image_size_x"]
 WINDOW_HEIGHT = cfg["SENSOR_CONFIG"]["DEPTH_RGB"]["ATTRIBUTE"]["image_size_y"]
@@ -26,7 +30,11 @@ def objects_filter(data):
     environment_objects = data["environment_objects"]
     agents_data = data["agents_data"]
     actors = data["actors"]
-    actors = [x for x in actors if x.type_id.find("vehicle") != -1 or x.type_id.find("walker") != -1]
+    actors = [
+        x
+        for x in actors
+        if x.type_id.find("vehicle") != -1 or x.type_id.find("walker") != -1
+    ]
     for agent, dataDict in agents_data.items():
         intrinsic = dataDict["intrinsic"]
         extrinsic = dataDict["extrinsic"]
@@ -39,9 +47,10 @@ def objects_filter(data):
 
         data["agents_data"][agent]["visible_environment_objects"] = []
         for obj in environment_objects:
-            kitti_datapoint, carla_datapoint = is_visible_by_bbox(agent, obj, image, depth_data, intrinsic, extrinsic)
+            kitti_datapoint, carla_datapoint = is_visible_by_bbox(
+                agent, obj, image, depth_data, intrinsic, extrinsic
+            )
             if kitti_datapoint is not None:
-                print("obj ",obj)
                 data["agents_data"][agent]["visible_environment_objects"].append(obj)
                 kitti_datapoints.append(kitti_datapoint)
                 carla_datapoints.append(carla_datapoint)
@@ -49,9 +58,10 @@ def objects_filter(data):
         data["agents_data"][agent]["visible_actors"] = []
 
         for act in actors:
-            kitti_datapoint, carla_datapoint = is_visible_by_bbox(agent, act, image, depth_data, intrinsic, extrinsic)
+            kitti_datapoint, carla_datapoint = is_visible_by_bbox(
+                agent, act, image, depth_data, intrinsic, extrinsic
+            )
             if kitti_datapoint is not None:
-                print("act ",act)
                 data["agents_data"][agent]["visible_actors"].append(act)
                 kitti_datapoints.append(kitti_datapoint)
                 carla_datapoints.append(carla_datapoint)
@@ -63,19 +73,37 @@ def objects_filter(data):
 
 
 def is_visible_by_bbox(agent, obj, rgb_image, depth_data, intrinsic, extrinsic):
-    obj_transform = obj.transform if isinstance(obj, carla.EnvironmentObject) else obj.get_transform()
+    obj_transform = (
+        obj.transform
+        if isinstance(obj, carla.EnvironmentObject)
+        else obj.get_transform()
+    )
     obj_bbox = obj.bounding_box
     if isinstance(obj, carla.EnvironmentObject):
-        vertices_pos2d = bbox_2d_from_agent(intrinsic, extrinsic, obj_bbox, obj_transform, 0)
+        vertices_pos2d = bbox_2d_from_agent(
+            intrinsic, extrinsic, obj_bbox, obj_transform, 0
+        )
     else:
-        vertices_pos2d = bbox_2d_from_agent(intrinsic, extrinsic, obj_bbox, obj_transform, 1)
+        vertices_pos2d = bbox_2d_from_agent(
+            intrinsic, extrinsic, obj_bbox, obj_transform, 1
+        )
     depth_image = depth_to_array(depth_data)
-    num_visible_vertices, num_vertices_outside_camera = calculate_occlusion_stats(vertices_pos2d, depth_image)
-    if num_visible_vertices >= MIN_VISIBLE_VERTICES_FOR_RENDER and num_vertices_outside_camera < MAX_OUT_VERTICES_FOR_RENDER:
+    num_visible_vertices, num_vertices_outside_camera = calculate_occlusion_stats(
+        vertices_pos2d, depth_image
+    )
+    if (
+        num_visible_vertices >= MIN_VISIBLE_VERTICES_FOR_RENDER
+        and num_vertices_outside_camera < MAX_OUT_VERTICES_FOR_RENDER
+    ):
         obj_tp = obj_type(obj)
         midpoint = midpoint_from_agent_location(obj_transform.location, extrinsic)
         bbox_2d = calc_projected_2d_bbox(vertices_pos2d)
-        rotation_y = get_relative_rotation_y(agent.get_transform().rotation, obj_transform.rotation) % math.pi
+        rotation_y = (
+            get_relative_rotation_y(
+                agent.get_transform().rotation, obj_transform.rotation
+            )
+            % math.pi
+        )
         ext = obj.bounding_box.extent
         truncated = num_vertices_outside_camera / 8
         if num_visible_vertices >= 6:
@@ -85,12 +113,31 @@ def is_visible_by_bbox(agent, obj, rgb_image, depth_data, intrinsic, extrinsic):
         else:
             occluded = 2
 
-        velocity = "0 0 0" if isinstance(obj, carla.EnvironmentObject) else\
-            "{} {} {}".format(obj.get_velocity().x, obj.get_velocity().y, obj.get_velocity().z)
-        acceleration = "0 0 0" if isinstance(obj, carla.EnvironmentObject) else \
-            "{} {} {}".format(obj.get_acceleration().x, obj.get_acceleration().y, obj.get_acceleration().z)
-        angular_velocity = "0 0 0" if isinstance(obj, carla.EnvironmentObject) else\
-            "{} {} {}".format(obj.get_angular_velocity().x, obj.get_angular_velocity().y, obj.get_angular_velocity().z)
+        velocity = (
+            "0 0 0"
+            if isinstance(obj, carla.EnvironmentObject)
+            else "{} {} {}".format(
+                obj.get_velocity().x, obj.get_velocity().y, obj.get_velocity().z
+            )
+        )
+        acceleration = (
+            "0 0 0"
+            if isinstance(obj, carla.EnvironmentObject)
+            else "{} {} {}".format(
+                obj.get_acceleration().x,
+                obj.get_acceleration().y,
+                obj.get_acceleration().z,
+            )
+        )
+        angular_velocity = (
+            "0 0 0"
+            if isinstance(obj, carla.EnvironmentObject)
+            else "{} {} {}".format(
+                obj.get_angular_velocity().x,
+                obj.get_angular_velocity().y,
+                obj.get_angular_velocity().z,
+            )
+        )
         # draw_3d_bounding_box(rgb_image, vertices_pos2d)
 
         kitti_data = KittiDescriptor()
@@ -110,18 +157,20 @@ def is_visible_by_bbox(agent, obj, rgb_image, depth_data, intrinsic, extrinsic):
         return kitti_data, carla_data
     return None, None
 
+
 def obj_type(obj):
     if isinstance(obj, carla.EnvironmentObject):
         return obj.type
     else:
-        if obj.type_id.find('walker') is not -1:
-            return 'Pedestrian'
-        if obj.type_id.find('vehicle') is not -1:
-            return 'Car'
+        if obj.type_id.find("walker") is not -1:
+            return "Pedestrian"
+        if obj.type_id.find("vehicle") is not -1:
+            return "Car"
         return None
 
+
 def get_relative_rotation_y(agent_rotation, obj_rotation):
-    """ 返回actor和camera在rotation yaw的相对角度 """
+    """返回actor和camera在rotation yaw的相对角度"""
 
     rot_agent = agent_rotation.yaw
     rot_car = obj_rotation.yaw
@@ -134,9 +183,11 @@ def bbox_2d_from_agent(intrinsic_mat, extrinsic_mat, obj_bbox, obj_transform, ob
         bbox_transform = carla.Transform(obj_bbox.location, obj_bbox.rotation)
         bbox = transform_points(bbox_transform, bbox)
     else:
-        box_location = carla.Location(obj_bbox.location.x-obj_transform.location.x,
-                                      obj_bbox.location.y-obj_transform.location.y,
-                                      obj_bbox.location.z-obj_transform.location.z)
+        box_location = carla.Location(
+            obj_bbox.location.x - obj_transform.location.x,
+            obj_bbox.location.y - obj_transform.location.y,
+            obj_bbox.location.z - obj_transform.location.z,
+        )
         box_rotation = obj_bbox.rotation
         bbox_transform = carla.Transform(box_location, box_rotation)
         bbox = transform_points(bbox_transform, bbox)
@@ -148,21 +199,23 @@ def bbox_2d_from_agent(intrinsic_mat, extrinsic_mat, obj_bbox, obj_transform, ob
 
 
 def vertices_from_extension(ext):
-    """ 以自身为原点的八个点的坐标 """
-    return np.array([
-        [ext.x, ext.y, ext.z],  # Top left front
-        [- ext.x, ext.y, ext.z],  # Top left back
-        [ext.x, - ext.y, ext.z],  # Top right front
-        [- ext.x, - ext.y, ext.z],  # Top right back
-        [ext.x, ext.y, - ext.z],  # Bottom left front
-        [- ext.x, ext.y, - ext.z],  # Bottom left back
-        [ext.x, - ext.y, - ext.z],  # Bottom right front
-        [- ext.x, - ext.y, - ext.z]  # Bottom right back
-    ])
+    """以自身为原点的八个点的坐标"""
+    return np.array(
+        [
+            [ext.x, ext.y, ext.z],  # Top left front
+            [-ext.x, ext.y, ext.z],  # Top left back
+            [ext.x, -ext.y, ext.z],  # Top right front
+            [-ext.x, -ext.y, ext.z],  # Top right back
+            [ext.x, ext.y, -ext.z],  # Bottom left front
+            [-ext.x, ext.y, -ext.z],  # Bottom left back
+            [ext.x, -ext.y, -ext.z],  # Bottom right front
+            [-ext.x, -ext.y, -ext.z],  # Bottom right back
+        ]
+    )
 
 
 def transform_points(transform, points):
-    """ 作用：将三维点坐标转换到指定坐标系下 """
+    """作用：将三维点坐标转换到指定坐标系下"""
     # 转置
     points = points.transpose()
     # [[X0..,Xn],[Y0..,Yn],[Z0..,Zn],[1,..1]]  (4,8)
@@ -192,25 +245,28 @@ def vertices_to_2d_coords(bbox, intrinsic_mat, extrinsic_mat):
 
 
 def vertex_to_world_vector(vertex):
-    """ 以carla世界向量（X，Y，Z，1）返回顶点的坐标 （4,1）"""
-    return np.array([
-        [vertex[0, 0]],  # [[X,
-        [vertex[0, 1]],  # Y,
-        [vertex[0, 2]],  # Z,
-        [1.0]  # 1.0]]
-    ])
+    """以carla世界向量（X，Y，Z，1）返回顶点的坐标 （4,1）"""
+    return np.array(
+        [
+            [vertex[0, 0]],  # [[X,
+            [vertex[0, 1]],  # Y,
+            [vertex[0, 2]],  # Z,
+            [1.0],  # 1.0]]
+        ]
+    )
 
 
 def calculate_occlusion_stats(vertices_pos2d, depth_image):
-    """ 作用：筛选bbox八个顶点中实际可见的点 """
+    """作用：筛选bbox八个顶点中实际可见的点"""
     num_visible_vertices = 0
     num_vertices_outside_camera = 0
 
     for y_2d, x_2d, vertex_depth in vertices_pos2d:
         # 点在可见范围中，并且没有超出图片范围
-        if MAX_RENDER_DEPTH_IN_METERS > vertex_depth > 0 and point_in_canvas((y_2d, x_2d)):
-            is_occluded = point_is_occluded(
-                (y_2d, x_2d), vertex_depth, depth_image)
+        if MAX_RENDER_DEPTH_IN_METERS > vertex_depth > 0 and point_in_canvas(
+            (y_2d, x_2d)
+        ):
+            is_occluded = point_is_occluded((y_2d, x_2d), vertex_depth, depth_image)
             if not is_occluded:
                 num_visible_vertices += 1
         else:
@@ -219,7 +275,12 @@ def calculate_occlusion_stats(vertices_pos2d, depth_image):
 
 
 def point_in_canvas(pos):
-    if (pos[0] >= 0) and (pos[0] < WINDOW_HEIGHT) and (pos[1] >= 0) and (pos[1] < WINDOW_WIDTH):
+    if (
+        (pos[0] >= 0)
+        and (pos[0] < WINDOW_HEIGHT)
+        and (pos[1] >= 0)
+        and (pos[1] < WINDOW_WIDTH)
+    ):
         return True
     return False
 
@@ -227,6 +288,7 @@ def point_in_canvas(pos):
 def point_is_occluded(point, vertex_depth, depth_image):
     y, x = map(int, point)
     from itertools import product
+
     neigbours = product((1, -1), repeat=2)
     is_occluded = []
     for dy, dx in neigbours:
@@ -241,13 +303,10 @@ def point_is_occluded(point, vertex_depth, depth_image):
 
 
 def midpoint_from_agent_location(location, extrinsic_mat):
-    """ 将agent在世界坐标系中的中心点转换到相机坐标系下 """
-    midpoint_vector = np.array([
-        [location.x],  # [[X,
-        [location.y],  # Y,
-        [location.z],  # Z,
-        [1.0]  # 1.0]]
-    ])
+    """将agent在世界坐标系中的中心点转换到相机坐标系下"""
+    midpoint_vector = np.array(
+        [[location.x], [location.y], [location.z], [1.0]]  # [[X,  # Y,  # Z,  # 1.0]]
+    )
     transformed_3d_midpoint = proj_to_camera(midpoint_vector, extrinsic_mat)
     return transformed_3d_midpoint
 
@@ -262,7 +321,7 @@ def camera_intrinsic(width, height):
 
 
 def proj_to_camera(pos_vector, extrinsic_mat):
-    """ 作用：将点的world坐标转换到相机坐标系中 """
+    """作用：将点的world坐标转换到相机坐标系中"""
     # inv求逆矩阵
     transformed_3d_pos = np.dot(inv(extrinsic_mat), pos_vector)
     return transformed_3d_pos
@@ -271,39 +330,49 @@ def proj_to_camera(pos_vector, extrinsic_mat):
 def proj_to_2d(camera_pos_vector, intrinsic_mat):
     """将相机坐标系下的点的3d坐标投影到图片上"""
     cords_x_y_z = camera_pos_vector[:3, :]
-    cords_y_minus_z_x = np.concatenate([cords_x_y_z[1, :], -cords_x_y_z[2, :], cords_x_y_z[0, :]])
+    cords_y_minus_z_x = np.concatenate(
+        [cords_x_y_z[1, :], -cords_x_y_z[2, :], cords_x_y_z[0, :]]
+    )
     pos2d = np.dot(intrinsic_mat, cords_y_minus_z_x)
     # normalize the 2D points
-    pos2d = np.array([
-        pos2d[0] / pos2d[2],
-        pos2d[1] / pos2d[2],
-        pos2d[2]
-    ])
+    pos2d = np.array([pos2d[0] / pos2d[2], pos2d[1] / pos2d[2], pos2d[2]])
     return pos2d
 
 
 def filter_by_distance(data_dict, dis):
     environment_objects = data_dict["environment_objects"]
     actors = data_dict["actors"]
-    for agent,_ in data_dict["agents_data"].items():
-        data_dict["environment_objects"] = [obj for obj in environment_objects if
-                                            distance_between_locations(obj.transform.location, agent.get_location())
-                                            <dis]
-        data_dict["actors"] = [act for act in actors if
-                                            distance_between_locations(act.get_location(), agent.get_location())<dis]
+    for agent, _ in data_dict["agents_data"].items():
+        data_dict["environment_objects"] = [
+            obj
+            for obj in environment_objects
+            if distance_between_locations(obj.transform.location, agent.get_location())
+            < dis
+        ]
+        data_dict["actors"] = [
+            act
+            for act in actors
+            if distance_between_locations(act.get_location(), agent.get_location())
+            < dis
+        ]
 
 
 def distance_between_locations(location1, location2):
-    return math.sqrt(pow(location1.x-location2.x, 2)+pow(location1.y-location2.y, 2))
+    return math.sqrt(
+        pow(location1.x - location2.x, 2) + pow(location1.y - location2.y, 2)
+    )
+
 
 def calc_projected_2d_bbox(vertices_pos2d):
-    """ 根据八个顶点的图片坐标，计算二维bbox的左上和右下的坐标值 """
+    """根据八个顶点的图片坐标，计算二维bbox的左上和右下的坐标值"""
     legal_pos2d = list(filter(lambda x: x is not None, vertices_pos2d))
     y_coords, x_coords = [int(x[0][0]) for x in legal_pos2d], [
-        int(x[1][0]) for x in legal_pos2d]
+        int(x[1][0]) for x in legal_pos2d
+    ]
     min_x, max_x = min(x_coords), max(x_coords)
     min_y, max_y = min(y_coords), max(y_coords)
     return [min_x, min_y, max_x, max_y]
+
 
 def degrees_to_radians(degrees):
     return degrees * math.pi / 180
--- utils/camera_manager.py
+++ utils/camera_manager.py
@@ -1,4 +1,3 @@
-
 # ==============================================================================
 # -- CameraManager -------------------------------------------------------------
 # ==============================================================================
@@ -9,48 +8,74 @@ import weakref
 import numpy as np
 import pygame
 
-class CameraManager(object):
-    """ Class for camera management"""
 
-    def __init__(self, parent_actor, hud):
+class CameraManager(object):
+    """Class for camera management"""
+
+    def __init__(self, parent_actor, hud, predictions: None):
         """Constructor method"""
         self.sensor = None
         self.surface = None
         self._parent = parent_actor
         self.hud = hud
         self.recording = False
+        self.predictions = predictions
         bound_y = 0.5 + self._parent.bounding_box.extent.y
         attachment = carla.AttachmentType
         self._camera_transforms = [
-            (carla.Transform(
-                carla.Location(x=-5.5, z=2.5), carla.Rotation(pitch=8.0)), attachment.SpringArm),
-            (carla.Transform(
-                carla.Location(x=1.6, z=1.7)), attachment.Rigid),
-            (carla.Transform(
-                carla.Location(x=5.5, y=1.5, z=1.5)), attachment.SpringArm),
-            (carla.Transform(
-                carla.Location(x=-8.0, z=6.0), carla.Rotation(pitch=6.0)), attachment.SpringArm),
-            (carla.Transform(
-                carla.Location(x=-1, y=-bound_y, z=0.5)), attachment.Rigid)]
+            (
+                carla.Transform(
+                    carla.Location(x=-5.5, z=2.5), carla.Rotation(pitch=8.0)
+                ),
+                attachment.SpringArm,
+            ),
+            (carla.Transform(carla.Location(x=1.6, z=1.7)), attachment.Rigid),
+            (
+                carla.Transform(carla.Location(x=5.5, y=1.5, z=1.5)),
+                attachment.SpringArm,
+            ),
+            (
+                carla.Transform(
+                    carla.Location(x=-8.0, z=6.0), carla.Rotation(pitch=6.0)
+                ),
+                attachment.SpringArm,
+            ),
+            (
+                carla.Transform(carla.Location(x=-1, y=-bound_y, z=0.5)),
+                attachment.Rigid,
+            ),
+        ]
         self.transform_index = 1
         self.sensors = [
-            ['sensor.camera.rgb', cc.Raw, 'Camera RGB'],
-            ['sensor.camera.depth', cc.Raw, 'Camera Depth (Raw)'],
-            ['sensor.camera.depth', cc.Depth, 'Camera Depth (Gray Scale)'],
-            ['sensor.camera.depth', cc.LogarithmicDepth, 'Camera Depth (Logarithmic Gray Scale)'],
-            ['sensor.camera.semantic_segmentation', cc.Raw, 'Camera Semantic Segmentation (Raw)'],
-            ['sensor.camera.semantic_segmentation', cc.CityScapesPalette,
-             'Camera Semantic Segmentation (CityScapes Palette)'],
-            ['sensor.lidar.ray_cast', None, 'Lidar (Ray-Cast)']]
+            ["sensor.camera.rgb", cc.Raw, "Camera RGB"],
+            ["sensor.camera.depth", cc.Raw, "Camera Depth (Raw)"],
+            ["sensor.camera.depth", cc.Depth, "Camera Depth (Gray Scale)"],
+            [
+                "sensor.camera.depth",
+                cc.LogarithmicDepth,
+                "Camera Depth (Logarithmic Gray Scale)",
+            ],
+            [
+                "sensor.camera.semantic_segmentation",
+                cc.Raw,
+                "Camera Semantic Segmentation (Raw)",
+            ],
+            [
+                "sensor.camera.semantic_segmentation",
+                cc.CityScapesPalette,
+                "Camera Semantic Segmentation (CityScapes Palette)",
+            ],
+            ["sensor.lidar.ray_cast", None, "Lidar (Ray-Cast)"],
+        ]
         world = self._parent.get_world()
         bp_library = world.get_blueprint_library()
         for item in self.sensors:
             blp = bp_library.find(item[0])
-            if item[0].startswith('sensor.camera'):
-                blp.set_attribute('image_size_x', str(hud.dim[0]))
-                blp.set_attribute('image_size_y', str(hud.dim[1]))
-            elif item[0].startswith('sensor.lidar'):
-                blp.set_attribute('range', '50')
+            if item[0].startswith("sensor.camera"):
+                blp.set_attribute("image_size_x", str(hud.dim[0]))
+                blp.set_attribute("image_size_y", str(hud.dim[1]))
+            elif item[0].startswith("sensor.lidar"):
+                blp.set_attribute("range", "50")
             item.append(blp)
         self.index = None
 
@@ -62,8 +87,13 @@ class CameraManager(object):
     def set_sensor(self, index, notify=True, force_respawn=False):
         """Set a sensor"""
         index = index % len(self.sensors)
-        needs_respawn = True if self.index is None else (
-            force_respawn or (self.sensors[index][0] != self.sensors[self.index][0]))
+        needs_respawn = (
+            True
+            if self.index is None
+            else (
+                force_respawn or (self.sensors[index][0] != self.sensors[self.index][0])
+            )
+        )
         if needs_respawn:
             if self.sensor is not None:
                 self.sensor.destroy()
@@ -72,12 +102,15 @@ class CameraManager(object):
                 self.sensors[index][-1],
                 self._camera_transforms[self.transform_index][0],
                 attach_to=self._parent,
-                attachment_type=self._camera_transforms[self.transform_index][1])
+                attachment_type=self._camera_transforms[self.transform_index][1],
+            )
 
             # We need to pass the lambda a weak reference to
             # self to avoid circular reference.
             weak_self = weakref.ref(self)
-            self.sensor.listen(lambda image: CameraManager._parse_image(weak_self, image))
+            self.sensor.listen(
+                lambda image: CameraManager._parse_image(weak_self, image)
+            )
         if notify:
             self.hud.notification(self.sensors[index][2])
         self.index = index
@@ -89,27 +122,30 @@ class CameraManager(object):
     def toggle_recording(self):
         """Toggle recording on or off"""
         self.recording = not self.recording
-        self.hud.notification('Recording %s' % ('On' if self.recording else 'Off'))
+        self.hud.notification("Recording %s" % ("On" if self.recording else "Off"))
 
     def render(self, display):
         """Render method"""
         if self.surface is not None:
-            display.blit(self.surface, (0, 0))
-
+            image = self.surface
+            print("Image type", type(image))
+            display.blit(image, (0, 0))
 
     def __init_calibration_matrix(self):
-        image_x = float(self.sensor.attributes['image_size_x'])
-        image_y = float(self.sensor.attributes['image_size_y'])
-        fov = float(self.sensor.attributes['fov'])
+        image_x = float(self.sensor.attributes["image_size_x"])
+        image_y = float(self.sensor.attributes["image_size_y"])
+        fov = float(self.sensor.attributes["fov"])
         calibration = np.identity(3)
         calibration[0, 2] = image_x / 2.0
         calibration[1, 2] = image_y / 2.0
-        calibration[0, 0] = calibration[1, 1] = image_x / (2.0 * np.tan(fov * np.pi / 360.0))
+        calibration[0, 0] = calibration[1, 1] = image_x / (
+            2.0 * np.tan(fov * np.pi / 360.0)
+        )
         self.image_width = image_x
         self.image_height = image_y
         self.fov = fov
         return calibration
-    
+
     def get_calibration_matrix(self):
         return self.calibration
 
@@ -118,13 +154,15 @@ class CameraManager(object):
         self = weak_self()
         if not self:
             return
-        if self.sensors[self.index][0].startswith('sensor.lidar'):
-            points = np.frombuffer(image.raw_data, dtype=np.dtype('f4'))
+        if self.sensors[self.index][0].startswith("sensor.lidar"):
+            points = np.frombuffer(image.raw_data, dtype=np.dtype("f4"))
             points = np.reshape(points, (int(points.shape[0] / 4), 4))
             lidar_data = np.array(points[:, :2])
             lidar_data *= min(self.hud.dim) / 100.0
             lidar_data += (0.5 * self.hud.dim[0], 0.5 * self.hud.dim[1])
-            lidar_data = np.fabs(lidar_data)  # pylint: disable=assignment-from-no-return
+            lidar_data = np.fabs(
+                lidar_data
+            )  # pylint: disable=assignment-from-no-return
             lidar_data = lidar_data.astype(np.int32)
             lidar_data = np.reshape(lidar_data, (-1, 2))
             lidar_img_size = (self.hud.dim[0], self.hud.dim[1], 3)
@@ -139,4 +177,4 @@ class CameraManager(object):
             array = array[:, :, ::-1]
             self.surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
         if self.recording:
-            image.save_to_disk('_out/%08d' % image.frame)
+            image.save_to_disk("_out/%08d" % image.frame)
