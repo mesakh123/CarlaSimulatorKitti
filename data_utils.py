c4e01792291496d692124126fa7e365fa5665d92
christianmasa <christian@linkernetworks.com>
2022-01-12 17:06:27 +0800

    test objType

--- data_utils.py
+++ data_utils.py
@@ -161,8 +161,10 @@ def is_visible_by_bbox(agent, obj, rgb_image, depth_data, intrinsic, extrinsic):
 
 def obj_type(obj):
     if isinstance(obj, carla.EnvironmentObject):
+        print("obj.type ", obj.type)
         return obj.type
     else:
+        print("obj.type_id ", obj.type_id)
         if obj.type_id.find("walker") is not -1:
             return "Pedestrian"
         if obj.type_id.find("vehicle") is not -1:
