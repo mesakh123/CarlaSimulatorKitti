519dd42cc334cbb7450fa94cce0446d507516803
christianmasa <christian@linkernetworks.com>
2022-01-19 11:32:15 +0800

    first commit

---
+++ SynchronousClient.py
@@ -0,0 +1,114 @@
+import random
+import numpy as np
+
+import carla
+from clientlib.camera_utils import CustomCamera
+from clientlib.lidar_utils import CustomLidar
+
+
+class SynchronousClient:
+    def __init__(self, cfg, args=None):
+        self.cfg = cfg
+        if args:
+            self.client = carla.Client(str(args.host), int(args.port))
+        else:
+            self.client = carla.Client("127.0.0.1", 2000)
+        self.client.set_timeout(4.0)
+        self.world = self.client.get_world()
+        self.manager = self.client.get_trafficmanager(8000)
+        self.set_synchronous_mode(True)
+
+        self.number_of_cars = int(self.cfg["CARLA_CONFIG"]["NUM_OF_VEHICLES"])
+        self.number_of_walkers = self(self.cfg["CARLA_CONFIG"]["NUM_OF_WALKERS"])
+
+        self.frames_per_second = 20
+
+        self.ego = None
+        self.spectator = None
+
+        self.image_x = self.cfg["SENSOR_CONFIG"]["RGB"]["ATTRIBUTE"]["image_size_x"]
+        self.image_y = self.cfg["SENSOR_CONFIG"]["RGB"]["ATTRIBUTE"]["image_size_y"]
+
+        self.fov = 90
+        self.sensor_tick = 0.1
+        self.tick = -1
+
+    def set_synchronous_mode(self, synchronous_mode):
+
+        settings = self.world.get_settings()
+        settings.synchronous_mode = synchronous_mode
+        settings.fixed_delta_seconds = 1 / self.frames_per_second
+        self.world.apply_settings(settings)
+        self.manager.set_synchronous_mode(synchronous_mode)
+
+    def setup_cars(self):
+
+        car_bps = self.world.get_blueprint_library().filter("vehicle.*")
+        car_bp_sample = []
+        for _ in range(self.number_of_cars):
+            car_bp_sample.append(random.choice(car_bps))
+        location = random.sample(
+            self.world.get_map().get_spawn_points(), self.number_of_cars
+        )
+        self.ego = self.world.spawn_actor(car_bps[0], location[0])
+        self.ego.set_autopilot(True, self.manager.get_port())
+        for i in range(1, self.number_of_cars):
+            current_car = self.world.spawn_actor(car_bp_sample[i], location[i])
+            current_car.set_autopilot(True, self.manager.get_port())
+
+    def update_spectator(self, transform=[-5.5, 0.0, 2.8, -15.0, 0.0, 0.0]):
+        """transform: = [x, y, z, pitch, yaw, roll]"""
+
+        specatator_vehicle_transform = carla.Transform(
+            location=carla.Location(*transform[0:3]),
+            rotation=carla.Rotation(*transform[3:6]),
+        )
+        specatator_vehicle_matrix = specatator_vehicle_transform.get_matrix()
+        vehicle_world_matrix = self.ego.get_transform().get_matrix()
+        specatator_world_matrix = np.dot(
+            vehicle_world_matrix, specatator_vehicle_matrix
+        )
+        specatator_world = np.dot(
+            specatator_world_matrix,
+            np.transpose(np.array([[*transform[0:3], 1.0]], dtype=np.dtype("float32"))),
+        )
+        specatator_rotation = self.ego.get_transform().rotation
+        specatator_rotation.pitch = -15
+        spectator_transform = carla.Transform(
+            location=carla.Location(*specatator_world[0:3, 0]),
+            rotation=specatator_rotation,
+        )
+        self.spectator.set_transform(spectator_transform)
+
+    def setup_spectator(self):
+
+        self.spectator = self.world.get_spectator()
+        self.update_spectator()
+
+    def setup_camera(self, transform, log_dir="training/data", suffix="", **options):
+        """transform: = [x, y, z, pitch, yaw, roll]"""
+
+        camera_location = carla.Location(*transform[0:3])
+        camera_rotation = carla.Rotation(*transform[3:6])
+        camera_transform = carla.Transform(
+            location=camera_location, rotation=camera_rotation
+        )
+        return CustomCamera(
+            self.world,
+            camera_transform,
+            self.ego,
+            log_dir,
+            suffix=suffix,
+            with_bbox=True,
+            **options
+        )
+
+    def setup_lidar(self, transform, log_dir="trainind/velodyne", **options):
+        """transform: = [x, y, z, pitch, yaw, roll]"""
+
+        lidar_location = carla.Location(*transform[0:3])
+        lidar_rotation = carla.Rotation(*transform[3:6])
+        lidar_transform = carla.Transform(
+            location=lidar_location, rotation=lidar_rotation
+        )
+        return CustomLidar(self.world, lidar_transform, self.ego, log_dir, **options)
---
+++ generator_test.py
@@ -0,0 +1,133 @@
+import sys
+import os
+import glob
+import argparse
+import logging
+
+from SynchronousClient import SynchronousClient
+from config import cfg_from_yaml_file
+
+
+def main(args):
+    cfg = cfg_from_yaml_file("configs.yaml")
+
+    try:
+        model = SynchronousClient(cfg)
+        tick = 0
+        world = model.world
+
+        while True:
+            tick += 1
+            world.tick()
+            model.update_spectator()
+    finally:
+        vehicles = world.get_actors().filter("vehicle.*")
+        for vehicle in vehicles:
+            vehicle.destroy()
+        model.set_synchronous_mode(False)
+
+
+if __name__ == "__main__":
+
+    argparser = argparse.ArgumentParser(description="CARLA Automatic Control Client")
+    argparser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        dest="debug",
+        help="Print debug information",
+    )
+    argparser.add_argument(
+        "--host",
+        metavar="H",
+        default="127.0.0.1",
+        help="IP of the host server (default: 127.0.0.1)",
+    )
+    argparser.add_argument(
+        "-p",
+        "--port",
+        metavar="P",
+        default=2000,
+        type=int,
+        help="TCP port to listen to (default: 2000)",
+    )
+
+    argparser.add_argument(
+        "--model-host",
+        default=None,
+        help="IP of the host server (default: None)",
+    )
+    argparser.add_argument(
+        "--model-port",
+        default=None,
+        type=int,
+        help="TCP port to listen to model_host (default: None)",
+    )
+
+    argparser.add_argument(
+        "--res",
+        metavar="WIDTHxHEIGHT",
+        default="1280x720",
+        help="Window resolution (default: 1280x720)",
+    )
+    argparser.add_argument(
+        "--sync", action="store_true", help="Synchronous mode execution"
+    )
+    argparser.add_argument(
+        "--filter",
+        metavar="PATTERN",
+        default="vehicle.*",
+        help='Actor filter (default: "vehicle.*")',
+    )
+    argparser.add_argument(
+        "-l",
+        "--loop",
+        action="store_true",
+        dest="loop",
+        help="Sets a new random destination upon reaching the previous one (default: False)",
+    )
+    argparser.add_argument(
+        "--predict",
+        action="store_true",
+        dest="predict",
+        help="Predict images on pygame (default: False)",
+    )
+    argparser.add_argument(
+        "-a",
+        "--agent",
+        type=str,
+        choices=["Behavior", "Basic"],
+        help="select which agent to run",
+        default="Behavior",
+    )
+    argparser.add_argument(
+        "-b",
+        "--behavior",
+        type=str,
+        choices=["cautious", "normal", "aggressive"],
+        help="Choose one of the possible agent behaviors (default: normal) ",
+        default="normal",
+    )
+    argparser.add_argument(
+        "-s",
+        "--seed",
+        help="Set seed for repeating executions (default: None)",
+        default=None,
+        type=int,
+    )
+    argparser.add_argument(
+        "--conf",
+        help="Set confidence of bbox (default: 0.6)",
+        default=0.6,
+        dest="conf",
+        type=float,
+    )
+
+    args = argparser.parse_args()
+
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    # logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
+
+    # logging.info('listening to server %s:%s', args.host, args.port)
+
+    main(args)
