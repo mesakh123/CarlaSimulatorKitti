4f11e367427a3b9aa2c743c38f2357ef0fd04ce4
christianmasa <christian@linkernetworks.com>
2022-01-20 18:45:51 +0800

    add only save kitti dataset option on generator

--- generator.py
+++ generator.py
@@ -2,12 +2,13 @@ from DataSave import DataSave
 from SynchronyModel import SynchronyModel
 from config import cfg_from_yaml_file
 from data_utils import objects_filter
+import argparse
 
 
-def main():
+def main(args):
     cfg = cfg_from_yaml_file("configs.yaml")
     model = SynchronyModel(cfg)
-    dtsave = DataSave(cfg)
+    dtsave = DataSave(cfg, args.kitti_only)
     try:
         model.set_synchrony()
         model.spawn_actors()
@@ -30,5 +31,12 @@ def main():
 
 
 if __name__ == "__main__":
-
-    main()
+    argparser = argparse.ArgumentParser(description="CARLA Automatic Control Client")
+    argparser.add_argument(
+        "--kitti-only",
+        action="store_true",
+        dest="kitti-only",
+        help="Only save KIITTI dataset format (images and KITTI labels)",
+    )
+    args = argparser.parse_args()
+    main(args)
